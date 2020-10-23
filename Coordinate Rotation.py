e220243c37ebfc5626a28645c4bbc6ee86c382b8
nipun0307 <71063756+nipun0307@users.noreply.github.com>
2020-10-24 01:04:50 +0530

    Update Coordinate Rotation.py

--- Coordinate Rotation.py
+++ Coordinate Rotation.py
@@ -1,44 +1,30 @@
-#Coordinate Rotation
-
 import math
 
+k=0.6072529350088814 #pre-computed constant (multiplying cos value for 32 iterations)
 
-def rotate(xy,angle,flag): #around origin
-    x,y=xy
-    (sin,cos)=compute_sin_cos(flag,angle,0,0,0.6072529350088814)
-    #X = x*cos - y*sin
-    #Y = x*sin + y*cos
+def rotate(point,angle,count):
+    x,y=point
     
-    Y = y*cos - x*sin
-    X = y*sin + x*cos
-    
-    return X, Y
-
-def compute_sin_cos(flag,angle,count,sin,cos):
     if(count==32):
-        if(flag==0):
-            return sin,cos
-        elif (flag==1):
-            return cos,-sin
-        else:
-            return -sin,-cos
-            
-        
+        x=k*x
+        y=k*y
+        return x,y
     else:
-        shifted_sin = sin/math.pow(2,count)
-        shifted_cos = cos/math.pow(2,count)
-        if (angle<0):
-            d=-1
-        else:
+        if(angle>=0):
             d=+1
-        cos_next = cos - (d*shifted_sin)
-        sin_next = sin + (d*shifted_cos)
-        angle_next = angle - (d*math.atan(math.pow(2,-count)))
+        else:
+            d=-1
+            
+        x_new=(x-(d*y*math.pow(2,-count)))
+        y_new=(y+(d*x*math.pow(2,-count)))
+        point_new=(x_new,y_new)
+        angle_new=angle-(d*math.atan(math.pow(2,-count)))
         count=count+1
-        return compute_sin_cos(flag,angle_next,count,sin_next,cos_next)
-
+        
+        return rotate(point_new,angle_new,count)
+        
 def _main():
-    #positive angle => CLOCKWISE rotation
+    
     x=float(input("enter the x coordinate: "))
     y=float(input("enter the y coordinate: "))
     deg_angle = float(input("Enter the angle in degrees: "))
@@ -51,17 +37,13 @@ def _main():
         if (deg_angle>90):
             deg_angle-=90
             flag = 2  ## Angle in 4th quadrant
-    angle = deg_angle/180*math.pi
+    angle = (deg_angle/180)*math.pi
     
     point=(x,y)
-    print(rotate(point,angle,flag))
+    print(rotate(point,angle,0))
     
  
     
 if __name__ == '__main__':
     _main()
-    
-        
-      
-    
-##init_cos = 0.6072529350088814
+
